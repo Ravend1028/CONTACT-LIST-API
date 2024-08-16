83b223a210048a9de1acd350c804d89b618b94e4
Ravend1028 <ravendavid.rd30@gmail.com>
2024-08-16 08:46:12 +0800

    initial commit

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+  class Database {
+    private $servername = "localhost";
+    private $dbname = "contacts";
+    private $username = "raven";
+    private $password = "123456";
+
+    protected function connect() {
+      try {
+        $dns = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname;
+        $conn = new PDO($dns, $this->username, $this->password);
+        // set the PDO error mode to exception
+        // study PDO ATTRIBUTES
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+    }
+  }
+
+?>
