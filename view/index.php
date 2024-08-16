7ec507a40200383b07b2352224768f75fccaf95b
Ravend1028 <ravendavid.rd30@gmail.com>
2024-08-16 23:17:28 +0800

    finished the ui

--- view/index.php
+++ view/index.php
@@ -8,8 +8,42 @@
   <link href="../styles/output.css" rel="stylesheet">
 </head>
 <body>
-  <h1 class="text-center text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <header class="bg-gray-100">
+    <div class="container mx-auto flex flex-col items-center justify-center p-6 space-y-3">
+      <h1 class="text-3xl font-medium text-center uppercase">
+        Contact List RESTful API-Based CRUD Operation
+      </h1>
+
+      <p class="font-normal">USING: </p>
+
+      <div class="flex flex-row items-center justify-center space-x-3">
+        <img class="h-24" src="../images/tailwind-logo.png" alt="tailwind-logo">
+        <img class="h-16" src="../images/php-logo.png" alt="php-logo">
+        <img class="h-14" src="../images/sql-logo.png" alt="mysql-logo">
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <div class="mx-auto container p-6 flex items-center justify-center flex-col space-y-5 space-x-0 md:flex-row md:space-x-5 md:space-y-0">
+      <button class="bg-amber-200 p-6 font-medium rounded-md">
+        SHOW CONTACT
+      </button>
+
+      <button class="bg-lime-400 p-6 font-medium rounded-md">
+        CREATE CONTACT
+      </button>
+
+      <button class="bg-fuchsia-400 p-6 font-medium rounded-md">
+        UPDATE CONTACT
+      </button>
+
+      <button class="bg-red-400 p-6 font-medium rounded-md">
+        DELETE CONTACT
+      </button>
+    </div>
+  </main>
+
+  <script src="../script/handler.js"></script>
 </body>
 </html>
