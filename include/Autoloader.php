8527d294245c1f82a09a943c4eb8d9251a61d70c
Ravend1028 <ravendavid.rd30@gmail.com>
2024-08-16 08:46:12 +0800

    initial commit

---
+++ include/Autoloader.php
@@ -0,0 +1,5 @@
+<?php
+
+  // SPL AUTOLOAD REGISTER
+
+?>
---
+++ model/Contact.php
@@ -0,0 +1,21 @@
+<?php
+
+  class Contact extends Database {
+    protected function getContacts() {
+      // PDO PREPARE STATEMENTS
+    }
+
+    protected function addToContacts() {
+      
+    }
+
+    protected function updateContacts() {
+      
+    }
+
+    protected function deleteToContacts() {
+      
+    }
+  }
+
+?>
